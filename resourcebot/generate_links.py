76116b7391443ceacd24c22e9cb0708669c0fc84
Vedank <vedank.pande@gmail.com>
2021-06-02 14:55:16 +0530

    cleaned up code

--- resourcebot/generate_links.py
+++ resourcebot/generate_links.py
@@ -26,36 +26,44 @@ class LinkGen:
             if idx == 0:
                 for param in subparam:
                     link += f"{param} "
+                    
             #search queries
             if idx == 1:
-                group = "("
-                for param in subparam:
-                    group += f"{param} OR "
-                group = group[:-4] + ")"
+                group = self.get_group(subparam) #add resource words
+
                 if provide:
-                    provide_group += '('
-                    for restrict in restrict_list:
-                        provide_group += f"{restrict} OR "
-                    provide_group = provide_group[:-4]
-                    provide_group += ')'
-                    print(provide_group)
+                    provide_group += self.get_group(restrict_list) #add provide words
+
                 if not provide:
                     group += " verified available"
+
                 else:
                     group += " " + provide_group + " "
                 link += f"{group}"
 
             #negate queries
             if idx == 2 and subparam[0]!='provide': #make sure 3rd param isn't the provide flag (if negate params aren't given)
-                for param in subparam:
-                    link += f" -\"{param}\""
+                negation = self.get_negate(subparam) #negate custom words
+                link += negation
 
         #negate if request
         if not provide:
-            for restrict in restrict_list:
-                link += f" -\"{restrict}\""
+            negation = self.get_negate(restrict_list)#negate need/require words
+            link += negation
         
         #direct to 'latest' tab
         return link + "&f=live"
 
+    def get_group(self,tokens):
+        group = "("
+        for token in tokens:
+            group += f"{token} OR "
+        group = group[:-4]+")" #remove final trailing OR substring
+        return group
+
+    def get_negate(self,tokens):
+        negate = ''
+        for token in tokens:
+            negate += f" -\"{token}\""
+        return negate
 
