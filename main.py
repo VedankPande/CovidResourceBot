0d2ca6f9c688e8361ecbf301c6d775d4caa110fb
Vedank <vedank.pande@gmail.com>
2021-06-03 12:52:05 +0530

    final commit

--- main.py
+++ main.py
@@ -13,26 +13,8 @@ since_id =1392801506370277384
 
 
 if __name__ == "__main__":
-    # request = "@ResourceBot (poone)  (oxygen,remdesivir,bed,food,ambulance) (extraargs)"
-    # params = [['poone','moombai'],['oxygen','remesdesvir','bed','food','ambulance'],['vedank']]
-    # print(tune(params))
-    # generator = LinkGen()
-    # print(generator.generate_link(tune(params)))
-
-    # request = '''Your request isn't formed properly, please try some of the below guidlines: \n
-    #                 1. A request has 4 parts (city)(resources)(negation words)(provide)\n
-    #                     the last two are optional\n
-    #                 2. Ensure that each part of your request is enclosed between parenthesis ()\n
-    #                 3. If you use the optional provide request, ensure that it is placed at\n
-    #                     the end of your request\n
-    #                 4. It is mandatory for separate words in the resources part to be \n
-    #                     separated by commas'''
-    # gen = LinkGen()
-    # print(gen.generate_link(tune(request)))
 
     api = Api(consumer_key,consumer_secret,access_token,access_token_secret,since_id)
-
-
 
     while(True):
         time.sleep(30)
