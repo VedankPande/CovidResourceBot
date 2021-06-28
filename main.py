ebe20c4bd9210c961d94c0805f9b3110db1bdd76
Vedank Pande <vedank.pande@gmail.com>
2021-06-28 11:53:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from resourcebot.autocorrect import tune
 
 import time
 
-consumer_key = "2Fa0aWQL6Jlyfx0vP2l6ROwIv"
-consumer_secret = "W0DCBRmLRL9m0r1RNIvr7vFUX1VikeFmhSvuqGxMJA5fsB3f2I"
-access_token = "896024023531913217-9UYqVvatdCsoOzLDJZlY8i6oIECD3ER"
-access_token_secret = "fr1dHeiyOoQZC0BgGrVUnzPwBne7uUyO2dO7DILASLhpX"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 since_id =1392801506370277384
 
 
