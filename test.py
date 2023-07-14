d27862a74cd9530217c0c29fc7455f1e30a4eeba
Vedank Pande <vedank.pande@gmail.com>
2023-07-14 17:24:34 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-""" test code here """ 
-import requests
-
-
-response = requests.post('https://api-ssl.bitly.com/v4/shorten',headers={"group_guid": "cdbf2bacdaf83d2a3fede50260c76eccaf7a6a5a",  "domain": "bit.ly",  "long_url": "https://dev.bitly.com" })
-print(response)
